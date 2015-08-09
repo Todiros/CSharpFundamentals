eecc9496afa2b8964a5e2235c64222fc8a281899
Xzahn <tony.d.borisov@gmail.com>
2015-08-09 10:13:53 +0300

    testing gitignore file by uploading single project

---
+++ CSharpBasics/Homework/2-Intro-Programming-Homework/PrintFirstAndLastName/PrintFirstAndLastName.cs
@@ -0,0 +1,17 @@
+﻿// 07. Create console application that prints your first and last name, each at a separate line.
+using System;
+
+namespace PrintFirstAndLastName
+{
+    class PrintFirstAndLastName
+    {
+        static void Main()
+        {
+            string firstName = "Tony";
+            string lastName = "Borisov";
+
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+        }
+    }
+}
---
+++ CSharpBasics/Homework/2-Intro-Programming-Homework/PrintNumbers/PrintNumbers.cs
@@ -0,0 +1,15 @@
+﻿//06. Write a program to print the numbers 1, 101 and 1001, each at a separate line. Name the program correctly. 
+using System;
+
+namespace PrintNumbers
+{
+    class PrintNumbers
+    {
+        static void Main()
+        {
+            Console.WriteLine(1);
+            Console.WriteLine(101);
+            Console.WriteLine(1001);
+        }
+    }
+}
