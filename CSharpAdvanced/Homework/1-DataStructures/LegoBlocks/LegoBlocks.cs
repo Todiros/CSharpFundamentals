b6ed121eff6a610b731896e37c463600d25fefa4
Tony <todiros@gmail.com>
2015-09-21 21:07:26 +0300

    Editing LegoBlock

---
+++ CSharpAdvanced/Homework/1-DataStructures/LegoBlocks/LegoBlocks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+
+class LegoBlocks
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int[][] firstLegoBlock = new int[n][];
+
+        for (int i = 0; i < n; i++)
+        {
+            firstLegoBlock[i] = Console.ReadLine().Split((char[])null, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        }
+        
+        int[][] secondLegoBlock = new int[n][];
+
+        for (int i = 0; i < n; i++)
+        {
+            secondLegoBlock[i] = Console.ReadLine().Split((char[])null, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        }
+    }
+}
